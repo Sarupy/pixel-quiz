f54d6b7ee748d028a11f8096ba7b5ebe344110e2
Caíque Santos <kaikysarupy@hotmail.com>
2024-10-07 22:42:59 -0300

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Layout from "./pages/Layout";
+// import Home from "./pages/Home";
+// import Blogs from "./pages/Blogs";
+// import Contact from "./pages/Contact";
+// import NoPage from "./pages/NoPage";
+import NavBar from './components/navBar';
+import './App.css';
+import { createTheme, ThemeProvider } from '@mui/material';
 
-function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
-}
+const simplifyChampionName = (name) => name.replaceAll(" ", "").replaceAll("'", "").replaceAll(".", "");
+
+const App = () => {
+
+  const theme = createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        main: '#ddd', // Defina sua cor primária aqui
+      },
+      secondary: {
+        main: '#00ccd4', // Você pode definir uma cor secundária também
+      },
+      background: {
+        default: '#aaa', // Cor de fundo padrão
+        paper: '#004A35', // Cor de fundo para componentes como Paper
+      },
+    },
+  });
+
+  return (
+    <>
+    <ThemeProvider theme={theme}>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<NavBar />}>
+          {/* <Route index element={<Home />} /> */}
+          {/* <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    </ThemeProvider>
+
+  </>
+  );
+};
 
 export default App;
