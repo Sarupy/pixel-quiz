da2c87fad7eba10cf63f0872544b450444bd7f15
Caíque Santos <kaikysarupy@hotmail.com>
2024-10-07 23:06:15 -0300

    t2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import React, { useEffect, useState } from 'react';
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
