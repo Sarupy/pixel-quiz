8d30063dd0558bc714406a31bfda635291f32d9b
Caíque Santos <kaikysarupy@hotmail.com>
2024-10-07 23:24:48 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-// import React from 'react';
-// import { useEffect, useState } from 'react';
-// import ReactDOM from "react-dom/client";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createTheme, ThemeProvider } from '@mui/material';
+
+// const simplifyChampionName = (name) => name.replaceAll(" ", "").replaceAll("'", "").replaceAll(".", "");
 
 const App = () => {
+
+  const theme = createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        main: '#ddd', // Defina sua cor primária aqui
+      },
+      secondary: {
+        main: '#00ccd4', // Você pode definir uma cor secundária também
+      },
+      background: {
+        default: '#aaa', // Cor de fundo padrão
+        paper: '#004A35', // Cor de fundo para componentes como Paper
+      },
+    },
+  });
+
   return (
-  
-      <h1>hello</h1>
-    
+    <>
+    <ThemeProvider theme={theme}>
+      <h1>Oi</h1>
+    {/* <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<NavBar />}>
+          <Route index element={<Home />} />
+          
+        </Route>
+      </Routes>
+    </BrowserRouter> */}
+    </ThemeProvider>
+
+  </>
   );
 };
 
